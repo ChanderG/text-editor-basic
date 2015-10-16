0a0e8fd7ba75f9d4b966ad35cae969b82804b118
ChanderG <chandergovind@gmail.com>
2015-10-16 22:05:40 +0530

    add text pane; implement basic menu
    simple format menu to change font sizes

--- editor.py
+++ editor.py
@@ -1,6 +1,7 @@
 # Simple text editor using tkinter
 
 import Tkinter as tk 
+import tkFont
 
 class Editor(tk.Frame):
 
@@ -12,7 +13,30 @@ class Editor(tk.Frame):
 
     def initUI(self):
         self.parent.title("text-editor-basic")
-        self.pack(fill=tk.BOTH, expand=1)
+
+        self.textWindow = tk.Text(font="18")
+        self.textWindow.pack(fill=tk.BOTH, expand=tk.YES)
+
+        menubar = tk.Menu(self)
+
+        formatmenu = tk.Menu(menubar)
+        formatmenu.add_command(label='Increase font size', command = self.incFontSize)
+        formatmenu.add_command(label='Decrease font size', command = self.decFontSize)
+        menubar.add_cascade(label = 'Format', menu = formatmenu)
+
+        self.parent.config(menu = menubar)
+
+    def incFontSize(self):
+        """ Increase editor font size by one unit."""
+        font = tkFont.Font(font = self.textWindow['font'])
+        font.config(size = str(int(font['size']+1)))
+        self.textWindow.config(font=font)
+
+    def decFontSize(self):
+        """ Increase editor font size by one unit."""
+        font = tkFont.Font(font = self.textWindow['font'])
+        font.config(size = str(int(font['size']-1)))
+        self.textWindow.config(font=font)
 
 def main():
     root = tk.Tk()
